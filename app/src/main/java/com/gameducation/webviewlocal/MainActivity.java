3d55bff8c52d559a884aa46315a560d035ca5eaa
nombre apellido <nada@nada.com>
2023-12-12 00:05:04 -0300

    Solo falta hacer el icono de la App

--- app/src/main/java/com/gameducation/webviewlocal/MainActivity.java
+++ app/src/main/java/com/gameducation/webviewlocal/MainActivity.java
@@ -7,6 +7,13 @@ import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
@@ -15,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // calendario
+        // Obtener la fecha actual
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMdd", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date());
+
+        // Construir la URL con el ancla
+        String url = "file:///android_asset/mais_semelhante_a_jesus.html#" + currentDate;
+
         webView = findViewById(R.id.myWebView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setBuiltInZoomControls(true);
@@ -25,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setAllowContentAccess(true);
         //
-        webView.loadUrl("file:///android_asset/index.html");
+        //webView.loadUrl("file:///android_asset/mais_semelhante_a_jesus.html#"+mes+dia);
+        webView.loadUrl(url);
 
 
     }
